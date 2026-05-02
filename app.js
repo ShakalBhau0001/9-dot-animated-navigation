2fce2b83ad7e0fab0d9c83eefa38c036b1fe019b
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-05-02 11:44:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const navigation = document.querySelector(".navigation");
 
+navigation.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+  navigation.style.transform = "scale(0.95)";
+  setTimeout(() => {
+    navigation.style.transform = "scale(1)";
+  }, 150);
+});
+
+document.addEventListener("click", (e) => {
+  if (!navigation.contains(e.target)) {
+    navigation.classList.remove("active");
+  }
+});
